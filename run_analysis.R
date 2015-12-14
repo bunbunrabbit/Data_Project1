83088c1abaeb66a00da3d6748db736574abc7718
bunbunrabbit <beccyk@gmail.com>
2015-12-14 12:41:14 -0800

    Updated to fix for friendly column names

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,20 @@
+## Data Project 1
+## 
+## Goal - to prepare tidy data that can be used for later analysis based on the data at
+##    http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+##
+## 1. Merges the training and the test sets to create one data set. 
+## 2. Extracts only the measurements on the mean and standard deviation 
+##      for each measurement. 
+## 3. Uses descriptive activity names to name the activities in the data set 
+## 4. Appropriately labels the data set with descriptive variable names. 
+## 5. From the data set in step 4, creates a second, independent tidy data set 
+##      with the average of each variable for each activity and each subject.
+
+##Set working directory
+setwd("~/Coursera/RData/RWorking/Data_Project1")
+
+##Set up variables for data files
 testXFile <- ".\\UCI HAR Dataset\\test\\X_test.txt"
 testYFile <- ".\\UCI HAR Dataset\\test\\Y_test.txt"
 testSubjectFile <- ".\\UCI HAR Dataset\\test\\subject_test.txt"
@@ -7,7 +24,6 @@ trainSubjectFile <- ".\\UCI HAR Dataset\\train\\subject_train.txt"
 activitiesFile <- ".\\UCI HAR Dataset\\activity_labels.txt"
 featuresFile <- ".\\UCI HAR Dataset\\features.txt"
 
-featuresdf <- read.table(featuresFile ) ## 561 observations
 activitiesdf <- read.table(activitiesFile) ## 1-6 
 
 ##Merges the training and the test sets to create one data set.
@@ -15,22 +31,34 @@ alldata <- rbind(read.table(testXFile ),
                         read.table(trainXFile ))
 Ydf <- rbind(read.table(testYFile),
                         read.table(trainYFile))
-subjectdf <- rbind(read.table(testSubjectFile),
+Subjectdf <- rbind(read.table(testSubjectFile),
                         read.table(trainSubjectFile))
 
 ##Extracts only the measurements on the mean and standard deviation for each measurement. 
-reg <- "std|mean"
-featuresdf <- featuresdf[grep(reg, featuresdf[,2]),]
+featuresdf <- read.table(featuresFile ) ## 561 observations
+reg <- "(std|mean[^F])"
+featuresdf <- featuresdf[grepl(reg, featuresdf[,2]),]
 alldata <- alldata[,featuresdf[,1]]
 
+##Update to human readable names
+featuresdf[,2] <- gsub("^tB","timedB",featuresdf[,2])
+featuresdf[,2] <- gsub("^tG","timedG",featuresdf[,2])
+featuresdf[,2] <- gsub("^fB","frequencyB",featuresdf[,2])
+featuresdf[,2] <- gsub("Jerk-","JerkSignal-",featuresdf[,2])
+featuresdf[,2] <- gsub("Acc","Acceleration",featuresdf[,2])
+featuresdf[,2] <- gsub("Acceleration-","AccelerationSignal-",featuresdf[,2])
+featuresdf[,2] <- gsub("Gyro","Gyroscope",featuresdf[,2])
+featuresdf[,2] <- gsub("Gyroscope-","GyroscopeSignal-",featuresdf[,2])
+featuresdf[,2] <- gsub("Mag-","Magnitude-",featuresdf[,2])
+
 ##Appropriately labels the data set with descriptive variable names. 
-names(alldata) <- featuresdf[,2]
+colnames(alldata) <- featuresdf[,2]
 
 ##Uses descriptive activity names to name the activities in the data set
-names(activitiesdf)[1] <- "activityID"
-names(activitiesdf)[2] <- "activity"
-names(Ydf)[1] <- "activityID"
-names(subjectdf)[1] <- "subjectID"
+colnames(activitiesdf)[1] <- "activityID"
+colnames(activitiesdf)[2] <- "activity"
+colnames(Ydf)[1] <- "activityID"
+colnames(Subjectdf)[1] <- "subjectID"
 
 ##Bind activity and subject
 Ydf <- cbind(Ydf, Subjectdf)
@@ -43,11 +71,11 @@ activitiessubjectdf <- merge(activitiessubjectdf, activitiesdf,by="activityID", 
 ##Add activity subject ID lookup column to alldata
 alldata$activitysubjectID <- Ydf[,"activitysubjectID"]
 
-##From the data set in step 4, creates a second, independent tidy data 
-##  set with the average of each variable for each activity and each subject.
+##Creates a second, independent tidy data set with the average of each variable 
+##  for each activity and each subject.
 tidydata <- do.call("rbind", 
     by(alldata, alldata[,"activitysubjectID"], simplify = TRUE,
-      function(x) colMeans(x[,1:79])
+            function(x) colMeans(x[,as.vector(featuresdf[,2])])
     ))
 
 ##update subject ID and activity in tidydata -- rowname holds the activitysubjectID
@@ -57,6 +85,3 @@ tidydata <- merge(activitiessubjectdf[,c("subjectID","activity","activitysubject
 
 ##Export tidy data
 write.table(tidydata,file="tidydata.txt",row.names=FALSE)
-
-##verification tests
-##splitdf <- split(alldata,alldata$activitysubjectID)
